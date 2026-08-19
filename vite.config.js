9da011873adbbd3ea9cf0ede45926f337c115243
Your Name <your@email.com>
2026-08-19 19:10:22 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/devops-portfolio/',
   plugins: [react(), tailwindcss()],
 })
